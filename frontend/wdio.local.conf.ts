8b0b8460e45b0c03f6956026740bb904d1ccc88e
Cade Ayres <ca469@kent.ac.uk>
2020-11-26 15:02:44 +0000

    Formatted wdio.local.conf.ts

--- frontend/wdio.local.conf.ts
+++ frontend/wdio.local.conf.ts
@@ -1,22 +1,22 @@
-const { config } = require('./wdio.shared.conf.ts')
+const {config} = require('./wdio.shared.conf.ts')
 
 exports.config = {
-  /**
-   * base config
-   */
-  ...config,
-  /**
-   * config for local testing
-   */
-  maxInstances: 1,
-  services: ['chromedriver'],
-  capabilities: [
-    {
-      browserName: 'chrome',
-      acceptInsecureCerts: true,
-      'goog:chromeOptions': {
-        args: ["--headless", '--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox']
-      }
-    },
-  ]
+    /**
+     * base config
+     */
+    ...config,
+    /**
+     * config for local testing
+     */
+    maxInstances: 1,
+    services: ['chromedriver'],
+    capabilities: [
+        {
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                args: ["--headless", '--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox']
+            }
+        },
+    ]
 }
