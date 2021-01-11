c5ebabc96d7dcdad310f307b73c204836ac6e07c
KyIe <kpeavot@hotmail.com>
2021-01-11 17:07:21 +0000

    Fixed unit test for text-field

--- frontend/tests/unit/components/text-field.spec.ts
+++ frontend/tests/unit/components/text-field.spec.ts
@@ -5,20 +5,20 @@ import TextValue from "@/models/form/TextValue";
 describe('TextField.vue', () => {
   let title: string;
   let guidance: string;
-  let textFieldValue: TextValue;
+  let textValue: TextValue;
   let wrapper: Wrapper<TextField>;
 
   beforeEach(() => {
     title = "Test question title";
     guidance = "Test question guidance";
-    textFieldValue = new TextValue("");
+    textValue = new TextValue("");
 
     wrapper = mount(TextField, {
       propsData: {
         level: 2,
         title: title,
         guidance: guidance,
-        textFieldValue: textFieldValue
+        textValue: textValue
       }
     })
   })
@@ -34,12 +34,12 @@ describe('TextField.vue', () => {
   it('text is updated when text entered in the input field',  () => {
     const testInput = "Test input";
 
-    expect(textFieldValue.value).toEqual("");
+    expect(textValue.value).toEqual("");
     expect((wrapper.find("input").element as HTMLInputElement).value).toEqual("");
 
     wrapper.find("input").setValue(testInput);
 
-    expect(textFieldValue.value).toEqual(testInput);
+    expect(textValue.value).toEqual(testInput);
     expect((wrapper.find("input").element as HTMLInputElement).value).toMatch(testInput);
   })
 })
