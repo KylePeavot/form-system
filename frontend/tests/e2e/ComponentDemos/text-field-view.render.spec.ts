d8a3ba25eb6fe86a19c2894c67b1dc093c5b58f7
KyIe <kpeavot@hotmail.com>
2021-01-12 15:26:13 +0000

    Changed tests to work in a less specific way than before

--- frontend/tests/e2e/ComponentDemos/text-field-view.render.spec.ts
+++ frontend/tests/e2e/ComponentDemos/text-field-view.render.spec.ts
@@ -11,30 +11,19 @@ describe("Text field demo page", () => {
     expect($('h1=Text field demo')).toBeDefined();
   })
   it("has 2 text field questions with the correct text", () => {
-    // expect($('h2=First name')).toBeDefined();
-    // expect($('p=Please enter your first name')).toBeDefined();
-    // expect($('input')).toBeDefined();
-    //
-    // expect($('h3=Surname')).toBeDefined();
-    // expect($('p=Please enter your surname')).toBeDefined();
-    // expect($('input')).toBeDefined();
+    // const question1ContainerElement = $('h2=First name').$('..');
 
-    let question1ContainerElement = $('h2=First name');
-    question1ContainerElement = question1ContainerElement.$('..');
+    // expect(question1ContainerElement).toBeDefined();
+    expect($('h2=First name')).toBeDefined();
+    expect($('p=Please enter your first name')).toBeDefined();
+    expect($('input')).toBeDefined();
 
+    // const question2ContainerElement = $('h3=Surname').$('..');
 
-    expect(question1ContainerElement).toBeDefined();
-    expect(question1ContainerElement.$('h2=First name')).toBeDefined();
-    expect(question1ContainerElement.$('p=Please enter your first name')).toBeDefined();
-    expect(question1ContainerElement.$('input')).toBeDefined();
-
-    let question2ContainerElement = $('h3=Surname').$('..');
-    // question2ContainerElement = question2ContainerElement.$('..');
-
-    expect(question2ContainerElement).toBeDefined();
-    expect(question2ContainerElement.$('h3=Surname')).toBeDefined();
-    expect(question2ContainerElement.$('p=Please enter your surname')).toBeDefined();
-    expect(question2ContainerElement.$('input')).toBeDefined();
+    // expect(question2ContainerElement).toBeDefined();
+    expect($('h3=Surname')).toBeDefined();
+    expect($('p=Please enter your surname')).toBeDefined();
+    expect($('input')).toBeDefined();
 
   })
 
