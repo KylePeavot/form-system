93ccfc08841a63395153062e8478e6492423d135
KyIe <kpeavot@hotmail.com>
2021-01-12 14:37:40 +0000

    WIP: Reverted tests and updated chromedriver and wdio

--- frontend/tests/e2e/ComponentDemos/text-field-view.render.spec.ts
+++ frontend/tests/e2e/ComponentDemos/text-field-view.render.spec.ts
@@ -11,28 +11,28 @@ describe("Text field demo page", () => {
     expect($('h1=Text field demo')).toBeDefined();
   })
   it("has 2 text field questions with the correct text", () => {
-    expect($('h2=First name')).toBeDefined();
-    expect($('p=Please enter your first name')).toBeDefined();
-    expect($('input')).toBeDefined();
+    // expect($('h2=First name')).toBeDefined();
+    // expect($('p=Please enter your first name')).toBeDefined();
+    // expect($('input')).toBeDefined();
+    //
+    // expect($('h3=Surname')).toBeDefined();
+    // expect($('p=Please enter your surname')).toBeDefined();
+    // expect($('input')).toBeDefined();
 
-    expect($('h3=Surname')).toBeDefined();
-    expect($('p=Please enter your surname')).toBeDefined();
-    expect($('input')).toBeDefined();
+    const question1ContainerElement = $('h2=First name').$('..');
 
-    // const question1ContainerElement = $('h2=First name').$('..');
-    //
-    // expect(question1ContainerElement).toBeDefined();
-    // expect(question1ContainerElement.$('h2=First name')).toBeDefined();
-    // expect(question1ContainerElement.$('p=Please enter your first name')).toBeDefined();
-    // expect(question1ContainerElement.$('input')).toBeDefined();
-    //
-    // const question2ContainerElement = $('h3=Surname').$('..');
-    //
-    // expect(question2ContainerElement).toBeDefined();
-    // expect(question2ContainerElement.$('h3=Surname')).toBeDefined();
-    // expect(question2ContainerElement.$('p=Please enter your surname')).toBeDefined();
-    // expect(question2ContainerElement.$('input')).toBeDefined();
-    //
+    expect(question1ContainerElement).toBeDefined();
+    expect(question1ContainerElement.$('h2=First name')).toBeDefined();
+    expect(question1ContainerElement.$('p=Please enter your first name')).toBeDefined();
+    expect(question1ContainerElement.$('input')).toBeDefined();
+
+    const question2ContainerElement = $('h3=Surname').$('..');
+
+    expect(question2ContainerElement).toBeDefined();
+    expect(question2ContainerElement.$('h3=Surname')).toBeDefined();
+    expect(question2ContainerElement.$('p=Please enter your surname')).toBeDefined();
+    expect(question2ContainerElement.$('input')).toBeDefined();
+
   })
 
 })
