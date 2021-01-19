1bdc254f745155dde8ab5aeb941dbb82c6400903
ij57 <ij57@kent.ac.uk>
2021-01-19 12:54:41 +0000

    Creation of the radio buttons.

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -4,7 +4,11 @@
       <CheckboxQuestion id="checkboxQuestion1" :level="2" title="Checkbox question" guidance="Guidance" v-model="inputValue"/>
       <br/>
       <CheckboxGroup title="Checkbox group" guidance="Select all that apply" id-prefix="cg" v-model="groupValues"/>
+      <br/>
+      <RadioGroup title="Radio group" guidance="Radio selection" v-model="radioValues"/>
     </BaseStyleLayout>
+
+
   </div>
 </template>
 
@@ -19,12 +23,22 @@ import CheckboxQuestion from "@/components/core/checkbox/CheckboxQuestion.vue";
 import Checkbox from "@/components/core/checkbox/Checkbox.vue";
 import CheckboxValue from "@/models/form/CheckboxValue";
 import CheckboxGroup from "@/components/core/checkbox/CheckboxGroup.vue";
+import RadioQuestion from "@/components/core/radio/RadioQuestions.vue";
+import Radio from "@/components/core/radio/Radio.vue";
+import RadioValue from "@/models/form/RadioValue";
+import RadioGroup from "@/components/core/radio/RadioGroup.vue";
+import RadioQuestions from "@/components/core/radio/RadioQuestions.vue";
+
 
 @Component({
   components: {
+    RadioQuestions,
     CheckboxGroup,
     Checkbox,
     CheckboxQuestion,
+    Radio,
+    RadioGroup,
+    RadioQuestion,
     BaseStyleLayout,
     Heading
   }
@@ -35,9 +49,14 @@ export default class Dashboard extends Vue {
   private page = CorePage.DASHBOARD;
   private inputValue = new CheckboxValue("Checkbox question", true);
   private groupValues = [
-      new CheckboxValue("I want to die", true),
-      new CheckboxValue("This is a cry for help", true),
-      new CheckboxValue("Something else", false)
+      new CheckboxValue("Option 1", true),
+      new CheckboxValue("Option 2", true),
+      new CheckboxValue("Option 3", false)
+  ]
+  private radioValues = [
+      new RadioValue("Yes",false),
+      new RadioValue("No",false),
+      new RadioValue("Maybe",false)
   ]
 
   mounted() {
