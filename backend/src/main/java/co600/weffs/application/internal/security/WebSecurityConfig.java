d9ff61af1a978949f9734ff18474e78c670c0699
Cade Ayres <ca469@kent.ac.uk>
2021-01-21 17:06:06 +0000

    Added profile context to cors bean

--- backend/src/main/java/co600/weffs/application/internal/security/WebSecurityConfig.java
+++ backend/src/main/java/co600/weffs/application/internal/security/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package co600.weffs.application.internal.security;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 
+@Profile("development")
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
