4e5ccb7d0a3d5b94d547ebb89c288e5713b92ae6
ij57 <ij57@kent.ac.uk>
2021-02-02 15:44:45 +0000

    Removed not null.

--- backend/src/main/resources/db/migration/V4__CREATE_QUESTION_DETAIL_TABLE.sql
+++ backend/src/main/resources/db/migration/V4__CREATE_QUESTION_DETAIL_TABLE.sql
@@ -2,12 +2,12 @@ CREATE TABLE question_detail(
     id INT AUTO_INCREMENT PRIMARY KEY,
     question_id INT REFERENCES question(id),
     title VARCHAR(4000) NOT NULL,
-    guidance VARCHAR(4000) NOT NULL,
-    question_type VARCHAR(100) NOT NULL,
-    parent_question_id INT NOT NULL,
+    guidance VARCHAR(4000),
+    question_type VARCHAR(4000) NOT NULL,
+    parent_question_id INT,
     order_number INT NOT NULL,
     last_updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     last_updated_by VARCHAR(4000) NOT NULL,
-    status_control VARCHAR(1) NOT NULL
+    status_control BOOL NOT NULL
 );
 CREATE INDEX idx_qd_question_id ON question_detail(question_id);
