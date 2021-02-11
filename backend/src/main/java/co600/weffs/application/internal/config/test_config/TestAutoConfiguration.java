a1af2cd317c51dd587ee36b8f6e6222d2ab52d56
Cade Ayres <ca469@kent.ac.uk>
2021-02-11 18:30:49 +0000

    Added dummy DataSource

--- backend/src/main/java/co600/weffs/application/internal/config/test_config/TestAutoConfiguration.java
+++ backend/src/main/java/co600/weffs/application/internal/config/test_config/TestAutoConfiguration.java
@@ -1,13 +1,72 @@
 package co600.weffs.application.internal.config.test_config;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("test")
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class TestAutoConfiguration {}
+public class TestAutoConfiguration {
+
+  @Bean
+  public DataSource testDataSource() {
+    return new DataSource() {
+      @Override
+      public Connection getConnection() throws SQLException {
+        return null;
+      }
+
+      @Override
+      public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+      }
+
+      @Override
+      public PrintWriter getLogWriter() throws SQLException {
+        return null;
+      }
+
+      @Override
+      public void setLogWriter(PrintWriter out) throws SQLException {
+
+      }
+
+      @Override
+      public void setLoginTimeout(int seconds) throws SQLException {
+
+      }
+
+      @Override
+      public int getLoginTimeout() throws SQLException {
+        return 0;
+      }
+
+      @Override
+      public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+      }
+
+      @Override
+      public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+      }
+
+      @Override
+      public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+      }
+    };
+  }
+
+}
