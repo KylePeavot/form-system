30b1566b455bcdfe05bf31e88aace4c0075957e0
ij57 <ij57@kent.ac.uk>
2021-03-05 13:20:05 +0000

    Fixed merge issues.

--- backend/src/test/java/co600/weffs/application/internal/services/FormDetailServiceTest.java
+++ backend/src/test/java/co600/weffs/application/internal/services/FormDetailServiceTest.java
@@ -1,18 +1,12 @@
 package co600.weffs.application.internal.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import co600.weffs.application.MockitoTest;
 import co600.weffs.application.internal.model.error.EntityNotFoundException;
 import co600.weffs.application.internal.model.form.Form;
 import co600.weffs.application.internal.model.form.FormDetail;
-import co600.weffs.application.internal.repository.FormDetailRepository;
-import co600.weffs.application.utils.forms.FormTestUtils;
 import co600.weffs.application.internal.repository.form.FormDetailRepository;
 import co600.weffs.application.internal.services.form.FormDetailService;
-import java.util.Optional;
+import co600.weffs.application.utils.forms.FormTestUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
